4b93b397785ad908a12996a8299c8371b5da2f6c
Erik Sandstedt <erik@sandstedt.eu>
2020-07-08 11:49:01 +0200

    namespace change

--- src/ConsoleApp/AppSettings.cs
+++ src/ConsoleApp/AppSettings.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace EliteBuckyball.ConsoleApp
+namespace EliteBuckyball.ConsoleApp.GenerateRoute
 {
     public class AppSettings
     {
